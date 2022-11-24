0bb00407d1034498d361920ca46cdd4dcdb1063d
lmbrjkvinci0619 <msv619@gmail.com>
2022-11-24 11:24:45 -0500

    Minor content/styling

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-let function displayConsole = (
+let a = 5;
 
-)
+console.log(a);
+
+let myGLobal = 10;
+
+function fun2() {
+    let output = "";
+    if (typeof myGLobal != "undefined") {
+        output += "myGlobal: " + myGLobal;
+    }
+    console.log(output);
+}
+
+fun2();
